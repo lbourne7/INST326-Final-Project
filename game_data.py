fabb02e9d29098eb6862dfc46678ecc7b75bd2c8
lbourne7 <lbourne7@terpmail.umd.edu>
2026-05-08 19:04:46 -0400

    Create game_data.py
    This file is required in order to pull the card data needed for the program. These files work in tandem and provides a fix for an earlier issue where we had hard coded data.

---
+++ game_data.py
@@ -0,0 +1,28 @@
+HAND_NAMES = {
+    1: "High Tide",
+    2: "Lucky Pair",
+    3: "Double Shell",
+    4: "Triple Threat",
+    5: "On a Roll",
+    6: "Red Flush",
+    7: "Full library",
+    8: "Quad Squad",
+    9: "Gold Flush",
+    10: "Testudo Flush"
+}
+
+VALUE_MAP = {
+        "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
+        "9": 9, "10": 10, "jack": 11, "queen": 12, "king": 13, "ace": 14
+}
+
+SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
+              "Jack", "Queen", "King", "Ace"]
+
+BET_LIMITS = {
+    "min": 1, 
+    "max": 3
+}
+
+BET_MULTIPLIERS = {1: 1, 2: 2, 3: 3}

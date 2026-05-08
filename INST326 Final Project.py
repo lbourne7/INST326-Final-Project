5f8eebb3787395aac489eb8025fa6dc0932232fb
Laurencia Aparin <laparin@terpmail.umd.edu>
2026-05-08 18:14:14 -0400

    chnaged docs string updated

--- INST326 Final Project.py
+++ INST326 Final Project.py
@@ -6,24 +6,23 @@ def take_players_bets(players):
     
     Take the players bets and keeps them for active player of the round. 
     
-    Atributes:
-        in the players 
-        name(str): the name of that player in a string 
-        folded(bool): if true then they dont contine if false then they do contintue
-        playing in the round. Determining if player is playing in current round 
-        or not.
-        current_bet(int): The amount that the player has in the round.
     Args:
-        players(list): A list of player object 
+        players(list): A list of player object. Each object has to have
+        'folded', 'name', and 'current_bet' attributes. 
         
-    returns:
+    Returns:
        int: The total sum of the bets collected during the round 
        
-     Side effects:
+    Side effects:
          Changes the current_bet attribute to the player object of each of them
          in the list. 
          Prints the error message if invalid and the enter the bet when being ran 
-         Waits until the user input is given to continue 
+         Waits until the user input is given to continue
+         
+    Raises:
+        ValueError: If a non-numeric string is entered by the user (this 
+        is caught and makes the user enter a correct input )
+           
     """
     total_players = 0
     print("Betting round")
@@ -52,26 +51,25 @@ def determine_winners(players, community_cards):
      
      Techniques used sequence unpacking and condtional expression
      
-     Attributes: 
-            player.name: used to see players name and idetify them from others 
-            and return a string
-            player.hand: Used for caculating the hand rank
-            player.folded: used to see if they player skips or not(means that they 
-            would be playing instead)
-     Args:
-         players(list): List of player objects and the players have the attrbuties
-         of the name(str) as a string and hand(list) as a list
-         the community cards is a list that is being used to show the shared/
-         common cards
+    Args:
+        players(list): List of player objects and the players have the attributes
+        of the name(str) as a string for each player and hand(list) as a list
+        folded (bool) if player is playing or not.
+        community cards (list): is a list that of string is being used to
+        show the shared/ common cards
         
-     Returns:
-         A tuple that conatins the winners_names and high_score where the 
-         winners_name(str) is a string of naes and the highest_score is the 
-         winner of the rank value.
+    Returns:
+        A tuple that contains the winners_names and highest_score where the 
+        winners_name(str) is a string of names and the highest_score is the 
+        winner of the rank value.
          
-     Side Effects:
+    Side Effects:
         It prints out the community cards and the players hand as they are playing
-        and the rank 
+        and their rank 
+    
+    Raises:
+        if player object is missing attributes
+    
     """
     
     highest_score = None
